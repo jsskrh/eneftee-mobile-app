21aee19eda167da7d720ecceadaaeda955c5c871
Jesse Akorah <jesseakorah@gmail.com>
2023-01-10 10:39:44 +0100

    Set up App.js

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+import { useFonts } from "expo-font";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +17,16 @@ const theme = {
 };
 
 const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
