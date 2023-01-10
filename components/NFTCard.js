a1b8d9123d4c6cb3fbce128a8921981a3b274b35
Jesse Akorah <jesseakorah@gmail.com>
2023-01-10 23:53:51 +0100

    Create NFT cards

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,10 +1,84 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { COLORS, SHADOWS, SIZES, assets } from "../constants";
+import { CircleButton, RectButton } from "./Button";
+import { EthPrice, NFTTitle, SubInfo } from "./SubInfo";
 
-const NFTCard = () => {
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>NFTCard</Text>
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+        {/* <CircleButton imgUrl={assets.heart} right={10} top={10} /> */}
+        <TouchableOpacity
+          style={{
+            width: 40,
+            height: 40,
+            backgroundColor: COLORS.white,
+            position: "absolute",
+            borderRadius: SIZES.extraLarge,
+            alignItems: "center",
+            justifyContent: "center",
+            right: 10,
+            top: 10,
+            ...SHADOWS.light,
+          }}
+          // onPress={handlePress}
+        >
+          <Image
+            source={assets.heart}
+            resizeMethod="contain"
+            style={{ width: 24, height: 24 }}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <SubInfo />
+
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subtitle={data.creator}
+          titleSize={SIZES.large}
+          subtitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
