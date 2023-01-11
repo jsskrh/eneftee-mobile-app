9047161838707ed783e4a7986ffd2d5e8d3451fc
Jesse Akorah <jesseakorah@gmail.com>
2023-01-11 02:43:32 +0100

    Create home header

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -68,7 +68,7 @@ export const People = () => {
     <View style={{ flexDirection: "row" }}>
       {[assets.person02, assets.person03, assets.person04].map(
         (imgUrl, index) => (
-          <ImageCmp imgUrl={imgUrl} index={index} />
+          <ImageCmp imgUrl={imgUrl} index={index} key={index} />
         )
       )}
     </View>
